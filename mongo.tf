d69cc0256e1077e93d87abde083bd2ba690da9ab
Jakub Sokołowski <jakub@status.im>
2020-05-21 10:50:48 +0200

    add SRV records for MongoDB servers
    https://docs.mongodb.com/manual/reference/connection-string/#connections-dns-seedlist
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- mongo.tf
+++ mongo.tf
@@ -38,3 +38,33 @@ module "webiny_db" {
   /* Plumbing */
   keypair_name    = aws_key_pair.jakub.key_name
 }
+
+resource "aws_route53_record" "webiny_db" {
+  zone_id = aws_route53_zone.webiny.zone_id
+  name    = "mongo-0${count.index+1}.db"
+  type    = "A"
+  ttl     = 30
+  records = [ module.webiny_db.public_ips[count.index] ]
+  count   = length(module.webiny_db.public_ips)
+}
+
+/* These allow for use of db.get.status.im in MongoDB URI */
+resource "aws_route53_record" "webiny_db_srv" {
+  zone_id = aws_route53_zone.webiny.zone_id
+  name    = "_mongodb._tcp.db"
+  type    = "SRV"
+  ttl     = 30
+  /* Order: priority weight port target */
+  records = [
+    for fqdn in aws_route53_record.webiny_db.*.fqdn:
+      "0 10 27017 ${fqdn}"
+  ]
+}
+
+resource "aws_route53_record" "webiny_db_txt" {
+  zone_id = aws_route53_zone.webiny.zone_id
+  name    = "db"
+  type    = "TXT"
+  ttl     = 30
+  records = ["replicaSet=webiny"]
+}
